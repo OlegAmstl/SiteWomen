45b61f8a9f370c2c90c441f6e55e3b9b976f1bd3
Oleg <amstl@mail.ru>
2024-02-04 18:01:22 +0700

    Update women_tags

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from women.models import Category, TagPost
 
@@ -7,11 +8,11 @@ register = template.Library()
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count('posts')).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('women/list_tags.html')
 def show_tags():
-    tags = TagPost.objects.all()
+    tags = TagPost.objects.annotate(total=Count('tags')).filter(total__gt=0)
     return {'tags': tags}
