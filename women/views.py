d475a75e662abee7c7567dc88f872ee1a2553b17
Oleg <amstl@mail.ru>
2024-02-05 14:41:58 +0700

    select_related for views index, show_category and show_tag_postlist.

--- women/views.py
+++ women/views.py
@@ -13,7 +13,7 @@ menu = [
 
 
 def index(request):
-    posts = Women.published.all()
+    posts = Women.published.all().select_related('cat')
     data = {
         'title': 'Главная страница',
         'menu': menu,
@@ -68,7 +68,7 @@ def show_post(request, post_slug):
 
 def show_category(request, cat_slug):
     category = get_object_or_404(Category, slug=cat_slug)
-    posts = Women.published.filter(cat_id=category.pk)
+    posts = Women.published.filter(cat_id=category.pk).select_related('cat')
 
     data = {
         'title': f'Рубрика: {category.name}',
@@ -81,7 +81,8 @@ def show_category(request, cat_slug):
 
 def show_tag_postlist(request, tag_slug):
     tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+    posts = tag.tags.filter(
+        is_published=Women.Status.PUBLISHED).select_related('cat')
 
     data = {
         'title': f'Тэг: {tag.tag}',
